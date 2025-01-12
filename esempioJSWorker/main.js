b11603ddcbdb8a07bd5a0322f221eba60368d7fa
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2025-01-12 11:54:35 +0100

    Update main.js
    .

--- esempioJSWorker/main.js
+++ esempioJSWorker/main.js
@@ -8,7 +8,8 @@ if (window.Worker) {
     elementoPrimoNumero.addEventListener("change", ()=>moltiplicatore.postMessage([elementoPrimoNumero.value, elementoSecondoNumero.value]));
     elementoSecondoNumero.addEventListener("change", ()=>moltiplicatore.postMessage([elementoPrimoNumero.value, elementoSecondoNumero.value]));
     
-    moltiplicatore.addEventListener("message",(e)=>{elementoRisultato.textContent = e.data;console.log("Message received from worker");});
+    moltiplicatore.addEventListener("message",(e)=>{    elementoRisultato.textContent = e.data;
+                                                        console.log("Message received from worker");
+                                                   });
   
 } else { console.log("Your browser doesn't support web workers."); }
-
